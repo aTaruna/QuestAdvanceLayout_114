eaa7a1a0100fdf742c7ecbde45acbef5c56b635c
aTaruna <aTaruna@users.noreply.github.com>
2025-10-23 12:09:59 +0700

    membuat column untuk menambah text

--- app/src/main/java/com/example/praktek4/ui/UI.kt
+++ app/src/main/java/com/example/praktek4/ui/UI.kt
@@ -10,18 +10,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-Import androidx.compose.ui.graphics.Color
 import com.example.praktek4.R
 
 
@@ -41,7 +43,7 @@ fun Activity(modifier: Modifier){
             .fillMaxWidth(1f)
             .padding(12.dp),
             colors = CardDefaults.cardColors(
-                containerColor = Color.Darkgray
+                containerColor = Color.DarkGray
             )){
             Row (){
                 val gambar = painterResource(id = R.drawable.logoumy)
@@ -50,6 +52,23 @@ fun Activity(modifier: Modifier){
                     contentDescription = "gambar",
                     modifier = Modifier.size(100.dp).padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column (){
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
             }
         }
     }
