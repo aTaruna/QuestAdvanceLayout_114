fb83c5164b0f7dc05cff48dbc243cfa4499e2788
aTaruna <aTaruna@users.noreply.github.com>
2025-10-23 11:56:17 +0700

    menambah layout untuk menambah gambar

--- app/src/main/java/com/example/praktek4/ui/UI.kt
+++ app/src/main/java/com/example/praktek4/ui/UI.kt
@@ -1,18 +1,22 @@
 package com.example.praktek4.ui
 
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,7 +43,14 @@ fun Activity(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.Darkgray
             )){
-            
+            Row (){
+                val gambar = painterResource(id = R.drawable.logoumy)
+                Image(
+                    painter = gambar,
+                    contentDescription = "gambar",
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+            }
         }
     }
 }
