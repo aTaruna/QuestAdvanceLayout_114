266ba902d959615f97b0a45c17f3c128f881b59d
aTaruna <aTaruna@users.noreply.github.com>
2025-10-23 11:53:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktek4/ui/UI.kt
@@ -0,0 +1,45 @@
+package com.example.praktek4.ui
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+Import androidx.compose.ui.graphics.Color
+import com.example.praktek4.R
+
+
+@Composable
+fun Activity(modifier: Modifier){
+    Column (modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
+        Text(stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+        Text(stringResource(R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Darkgray
+            )){
+            
+        }
+    }
+}
